a5e085fab97b0fd8206e5ba196c2e9047572bbe1
AxelWard <ajweagle@hotmail.com>
2018-10-08 18:49:27 -0600

    GIT Creation

---
+++ src/container/Driver.java
@@ -0,0 +1,26 @@
+package container;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        System.out.println("Running Graph Application\n");
+
+        menu();
+
+        System.out.print("Please make sure to put file in inputFiles folder\n\n\nFile path to read from: ");
+
+        Scanner input = new Scanner(System.in);
+        String path = input.nextLine();
+
+        File inputFile = new File(System.)
+    }
+
+    private static void menu() {
+        System.out.println("Menu\n----");
+        System.out.println("1. Read a graph in from a file");
+    }
+}
